150114ef0d7680ab006b6157cdf0ae5cf78a8777
Deepb112005 <deepbhatti112@gmail.com>
2026-08-03 19:37:51 +0530

    feature(service) : Added the delete service for the category model data
    	- added the controller for delete endpoint
    	- added the interface method and teh implementationg class for that

--- sb-ecom/src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
@@ -3,10 +3,7 @@ package com.ecommerce.sbecom.controller;
 import com.ecommerce.sbecom.model.Category;
 import com.ecommerce.sbecom.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +27,11 @@ public class CategoryController {
     public String createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return "category resource created";
+    }
+
+    @DeleteMapping("/api/admin/categories/{categoryId}")
+    public String deleteCategory(@PathVariable Long categoryId) {
+        return categoryService.deleteCategory(categoryId);
     }
 
 }
--- sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class CategoryServiceImpl implements CategoryService {
 
     List<Category> categories = new ArrayList<>();
-    private Long nextId  = 1L ;
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -21,6 +21,18 @@ public class CategoryServiceImpl implements CategoryService {
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
         categories.add(category);
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst().orElse(null);
+        if (category == null)
+            return "category not found";
+
+        categories.remove(category);
+        return "category with categoryId : " +categoryId + " removed ";
     }
 
 }
