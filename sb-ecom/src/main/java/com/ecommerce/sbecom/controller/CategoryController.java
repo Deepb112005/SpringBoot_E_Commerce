227d590a4683bc1379274dc5d28ea371848c9078
Deepb112005 <deepbhatti112@gmail.com>
2026-08-04 12:56:54 +0530

    feature(response entity) : Added the proper response Entity
    	- throw the responseStatusException for resurce not found in service layer
    	- handled that exception and respond with proper status code

--- sb-ecom/src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/controller/CategoryController.java
@@ -3,7 +3,10 @@ package com.ecommerce.sbecom.controller;
 import com.ecommerce.sbecom.model.Category;
 import com.ecommerce.sbecom.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,8 +33,12 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable Long categoryId) {
-        return categoryService.deleteCategory(categoryId);
+    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
+        try{
+            return new ResponseEntity<>(categoryService.deleteCategory(categoryId) , HttpStatus.OK);
+        }catch (ResponseStatusException e ){
+            return new ResponseEntity<>(e.getReason() , e.getStatusCode());
+        }
     }
 
 }
--- sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecommerce.sbecom.service;
 
 import com.ecommerce.sbecom.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,10 +30,7 @@ public class CategoryServiceImpl implements CategoryService {
     public String deleteCategory(Long categoryId) {
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst().orElse(null);
-        if (category == null)
-            return "category not found";
-
+                .findFirst().orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND , "resource not found "));
         categories.remove(category);
         return "category with categoryId : " +categoryId + " removed ";
     }
