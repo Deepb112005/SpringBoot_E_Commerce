f37f8e46bb02d5bec8d9deddb493f375d2b3a0ff
Deepb112005 <deepbhatti112@gmail.com>
2026-08-08 18:37:38 +0530

    feat(category): add pagination to getAllCategories endpoint
    - Add pageNumber and pageSize query params to the GET endpoint
    - Build a Pageable via PageRequest.of() in the service layer
    - Use categoryRepository.findAll(pageable) instead of fetching all rows

--- sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/service/CategoryServiceImpl.java
@@ -8,6 +8,9 @@ import com.ecommerce.sbecom.payload.CategoryResponse;
 import com.ecommerce.sbecom.repository.CategoryRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,8 +28,11 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public CategoryResponse getAllCategories() {
-        List<Category> categories = categoryRepository.findAll();
+    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize) {
+        Pageable pageDetails = PageRequest.of(pageNumber, pageSize);
+        Page<Category> categoryPage = categoryRepository.findAll(pageDetails);
+
+        List<Category> categories = categoryPage.getContent();
         if (categories.isEmpty())
             throw new APIException("No Category till now ...");
 
