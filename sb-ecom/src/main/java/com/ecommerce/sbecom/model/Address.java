f0db77758fe001a6f1acbc5c163a9e058c1d9f1c
Deepb112005 <deepbhatti112@gmail.com>
2026-08-26 17:54:20 +0530

    feat(security): scaffold User, Role, and Address entities
    - Add User entity with userName, email, and password fields
    - Add Role entity with roleId and roleName (AppRole enum)
    - Add AppRole enum with ROLE_USER, ROLE_ADMIN, ROLE_SELLER
    - Add empty Address entity as a placeholder

---
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/model/Address.java
@@ -0,0 +1,15 @@
+package com.ecommerce.sbecom.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+}
---
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/model/Role.java
@@ -0,0 +1,26 @@
+package com.ecommerce.sbecom.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name", length = 20)
+    private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+}
