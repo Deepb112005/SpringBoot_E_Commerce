ddecfed23e3ad89fa1ac11cfc4f76882d005b5ed
Deepb112005 <deepbhatti112@gmail.com>
2026-08-26 18:37:30 +0530

    feat(model): add many-to-many relationship between User and Role
    - Add roles field to User entity via @ManyToMany
    - Configure join table user_role with user_id/role_id columns
    - Use EAGER fetch with PERSIST/MERGE cascade for roles
    - Initialize roles as HashSet to avoid null collection issues

--- sb-ecom/src/main/java/com/ecommerce/sbecom/model/User.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/model/User.java
@@ -5,7 +5,12 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -29,6 +34,16 @@ public class User {
     @NotBlank
     @Size(max = 120)
     private String password;
+
+    @Getter
+    @Setter
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 
 
     public User(String email, String password, String userName) {
