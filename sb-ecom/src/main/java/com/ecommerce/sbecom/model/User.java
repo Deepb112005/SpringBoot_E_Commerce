cc4e24eb4199b747440f9ed8eac8939ca5ff1b72
Deepb112005 <deepbhatti112@gmail.com>
2026-08-31 17:54:24 +0530

    feat(cart): add Cart and CartItem entities with product/user relationships
    - Add Cart entity with one-to-one User mapping and one-to-many CartItem mapping
    - Add CartItem entity linking cart and product with quantity, discount and product price
    - Map Product to CartItem collection for cart-product relationships
    - Map User to Cart with cascade and orphan removal

--- sb-ecom/src/main/java/com/ecommerce/sbecom/model/User.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/model/User.java
@@ -73,6 +73,15 @@ public class User {
     private Set<Product> products;
 
 
+    @ToString.Exclude
+    @OneToOne(
+            mappedBy = "user",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            orphanRemoval = true
+    )
+    private Cart cart;
+
+
     public User(String userName, String email, String password) {
         this.email = email;
         this.password = password;
